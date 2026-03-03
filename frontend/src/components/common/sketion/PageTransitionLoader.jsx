fc642a9ef990087efa55fe60db9067e537bc73da
shibin kp <shibinkp1012@gmail.com>
2026-03-03 20:48:04 +0530

    the errors are fixed in last stage

---
+++ frontend/src/components/common/sketion/PageTransitionLoader.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import PageSkeleton from "./PageSkeleton";
+
+const PageTransitionLoader = ({ children }) => {
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 300); // small delay for smooth UX
+
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
+
+  if (loading) {
+    return <PageSkeleton />;
+  }
+
+  return children;
+};
+
+export default PageTransitionLoader;
